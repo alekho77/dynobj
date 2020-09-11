9c3bce11109b3bf5480425b9f7486e907e63573e
alekho <ahozin@hotmail.com>
2020-09-11 18:47:04 +0300

    Add doxygen target for dynobj project

--- include/dynobj/object_host.h
+++ include/dynobj/object_host.h
@@ -26,9 +26,61 @@ namespace dynobj {
 */
 class IDynamicObjectHost : public IRefCounter {
  public:
+    /*!
+        \brief Safely creates an instance of the host object.
+
+        \return intrusive smart pointer to binary-safe pointer
+    */
     static boost::intrusive_ptr<IDynamicObjectHost> createInstance() {
         return createInstancePrivate();
     }
+
+    /*!
+        \brief Loads a context from config string and then runs it.
+
+        Contexts are stored in LIFO ordered container.
+        The first loaded context is most major and destroyed in the last place.
+
+        \param config_json string with json-style configuration of a context
+        \par
+            Format of context configuration:
+            \code{.json}
+            {
+                "name" : "<context name>",
+                "objects" : [
+                    <array of objects>
+                ]
+            }
+            \endcode
+            Configuration of every object in the array should have the following format:
+            \code{.json}
+            {
+                "module" : "<library name>",
+                "name"   : "<object class name>"
+                "id"     : "<unique identifier>"
+                "connections" : [
+                    <array of object connections>
+                ]
+            }
+            \endcode
+
+        \return \c true if the context was loaded successfully.
+    */
+    virtual bool loadContext(const char* config_json) = 0;
+
+    /*!
+        \brief Drops last loaded context.
+
+        \see loadContext.
+
+        \return `true` if a context was dropped.
+        \par
+            For example you can release all contexts with following code:
+            \code{.cpp}
+            while(host_object->dropContext());
+            \endcode
+    */
+    virtual bool dropContext() = 0;
 
  private:
     static IDynamicObjectHost* createInstancePrivate();
