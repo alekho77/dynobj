05b95719ad8769465eaaab4eb57236bafc9e3539
alekho <ahozin@hotmail.com>
2020-12-11 12:08:49 +0300

    context sketch

---
+++ include/dynobj/context.h
@@ -0,0 +1,16 @@
+//
+// Dynamic Loaded Object Toolkit
+//
+
+/*!
+    \file
+    \brief  Context interface declaration to hold dynamic loaded objects.
+    \author Aleksei Khozin <ahozin@hotmail.com>
+    \date   September 2020
+
+    The file contains declaration of dynamic loaded objects context interface.
+*/
+
+#pragma once
+
+namespace dynobj {}
